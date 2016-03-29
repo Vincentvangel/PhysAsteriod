d365c8d66e7a77035fd333c83b79a674a52ea792
Vincentvangel <vmvangel@neiu.edu>
2016-03-29 03:59:14 -0500

    Update Simulation.py
    moved in the mores potential and optimized the code a bit.

--- Simulation.py
+++ Simulation.py
@@ -10,33 +10,49 @@ scene = display(title="Simulation", width=1.5*win, height=win, x=0, y=0,
 
 #functions, marked by def then name and imputs
 #funtions can be called and send back information
+G=6.7e-11
+alpha = 2
+req = 1.1
+
 
 def Islinked (n,amount):#defineing "linked" particals, much like a chemical bond.
     for l in range(0,active):
         for a in range(l+1,amount):
             j=mag(n[l].pos-n[a].pos)
-            if(j<1):
+            if(j<1.2):
                 n[l].link[a]=true
                 n[a].link[l]=true
+            else:
+                n[l].link[a]=false
+                n[a].link[l]=false
                 
-
-def Forcelinked (p1,p2):#the force that will be applied between two linked particals
-    d=p1.pos-p2.pos
+            
+def Forcelinked (l,a,j,dis):#the force that will be applied between two linked particals
+    d=n[l].pos-n[a].pos
     x=mag(d)
-    unitv=d/x
-    f=(cos(5*x)+1/(5*x))*12
+    unitv=j/dis
+    #f=(-x+1/x)*12
+
+    arg = exp(-2*alpha*(dis-req))-exp(-alpha*(dis-req))#OLD MORSE
+    Fgrav=(2*alpha*G*(n[l].mass*n[a].mass)/pow(1.1,2)*arg)*unitv
+
+
+
+    n[l].p+=Fgrav
+    n[a].p+=-Fgrav
+    if(dis>1.2):#bond length
+        n[l].link[a]=false
+        n[a].link[l]=false
+    print(d)          
+
+def Forceunlinked (p1,p2,j,dis):#force applied to unlinked particals
+
+    x=dis+1
+    unitv=j/dis
+    f=1/(x**2)
     p1.p+=f*unitv
     p2.p+=-f*unitv
-    return f;                
 
-def Forceunlinked (p1,p2):#force applied to unlinked particals
-    d=p1.pos-p2.pos
-    x=mag(d)
-    unitv=d/x
-    f=(cos(5*x)+1/(5*x))*12
-    p1.p+=f*unitv
-    p2.p+=-f*unitv
-    return f;
 
 
 active=7#total particals in the scene, this is important to have at the right number
@@ -52,7 +68,7 @@ d= [ (0.7430002202,0.2647603899,-0.0468575389)#the vectors for object 1
 
 for x in range(0,6):#buiiding object d
     n[x]=sphere(pos=d[x],color=(1,1,.5),make_trail=False,
-            radius=.7,mass=90,p=vector(20,0,0),Potential=0,v=0,link =[bool]*active)
+            radius=.7,mass=900,p=vector(200,0,0),Potential=0,v=0,link =[bool]*active)
     n[x].v = n[x].p/n[x].mass
     n[x].pos+=(-10,0,.3)#starting point of objects
     n[x].make_trail=True
@@ -60,26 +76,26 @@ for x in range(0,6):#buiiding object d
 
 
 n[6]=sphere(pos=(2,1,0),color=(1,1,.5),make_trail=False,# just a single partical
-            radius=.7,mass=90,p=vector(-20,0,0),Potential=0,v=0,link =[bool]*active)
+            radius=.7,mass=900,p=vector(-200,0,0),Potential=0,v=0,link =[bool]*active)
 n[6].v = n[6].p/n[6].mass
 
 
 j=0
-t=0
+t=0  
 dt=.1
-
+Islinked(n,active)
 while(t < 250):
     rate(60)
-    Islinked(n,active)
     for m in range(0,active):#changing postion
         n[m].v = 0.5*(n[m].v + n[m].p/n[m].mass)
         n[m].pos+=n[m].v*dt
     for l in range(0,active):#Calculations of force between all objects
         for a in range(l+1,active):
-            j=mag(n[l].pos-n[a].pos)
+            j=n[l].pos-n[a].pos
+            dis=mag(j)
 
-            if(n[l].link[a]==true):Forcelinked(n[l],n[a])#calling functions
-            else: Forceunlinked(n[l],n[a])
+            if(n[l].link[a]==true):Forcelinked(l,a,j,dis)#calling functions
+            else: Forceunlinked(n[l],n[a],j,dis)
             
 
     t += dt
